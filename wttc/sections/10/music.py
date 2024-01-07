a8662807193fe9977fdebc2f474da3e1a77da523
Trevor Bača <trevor.baca@gmail.com>
2024-01-06 22:17:28 -0500

    Cleaned up music.py files.

--- wttc/sections/10/music.py
+++ wttc/sections/10/music.py
@@ -390,6 +390,7 @@ def make_score(first_measure_number, previous_persistent_indicators):
         len(meters()),
         library.voice_abbreviations,
     )
+    # library.check_material_annotations(score)
     fl(cache["fl"])
     ob(cache["ob"])
     gt1(cache["gt1"])
