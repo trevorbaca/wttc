77d784e906ebe2153a66fbd826a86444b44a0557
Trevor Bača <trevor.baca@gmail.com>
2024-04-17 17:58:11 -0400

    Cleaned up music.py files.

--- wttc/sections/24/music.py
+++ wttc/sections/24/music.py
@@ -842,6 +842,7 @@ def persist_score(score, environment):
         environment,
         library.manifests,
         do_not_color_repeat_pitch_classes=True,
+        do_not_error_on_not_yet_pitched=True,
         global_rests_in_topmost_staff=True,
     )
     baca.section.activate_tags(
