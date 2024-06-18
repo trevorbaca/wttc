5c0f4a3a12ea49e2b9112a19660e83961a76287e
Trevor Bača <trevor.baca@gmail.com>
2024-06-18 13:46:32 -0400

    Cleaned up music.py files.

--- wttc/sections/24/music.py
+++ wttc/sections/24/music.py
@@ -903,7 +903,7 @@ def make_layout(environment):
         default=(1, 20),
         lax_spacing_section=[11, 12],
     )
-    lilypond_file, bol_measure_numbers = baca.build.write_layout_ily(
+    return baca.build.write_layout_ily(
         breaks,
         environment.metadata["time_signatures"],
         first_measure_number=environment.first_measure_number,
@@ -925,7 +925,11 @@ def main():
         )
         persist_score(score, environment)
     if environment.arguments.layout:
-        make_layout(environment)
+        lilypond_file, bol_measure_numbers = make_layout(environment)
+        baca.build.persist_layout_ily(environment.section_directory, lilypond_file)
+        baca.build.write_bol_metadata(
+            environment.section_directory, bol_measure_numbers
+        )
 
 
 if __name__ == "__main__":
