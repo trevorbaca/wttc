4c2f81c3b8bb1f526ef104df8f50d42463532c5c
Trevor Bača <trevor.baca@gmail.com>
2024-07-04 10:15:05 +0200

    Cleaned up parts.

---
+++ wttc/builds/letter-parts/guitar-1/layout.py
@@ -0,0 +1,132 @@
+import baca
+
+
+def main(environment):
+    distances = (6,)
+    half = int(26 / 2)
+    y_offset_1 = 12
+    y_offset_2 = 38
+    y_offset_3 = 64
+    y_offset_4 = 90
+    y_offset_5 = 116
+    y_offset_6 = 142
+    base = 12
+    width = 32
+    y_offset_5_1 = base + 0 * width
+    y_offset_5_2 = base + 1 * width
+    y_offset_5_3 = base + 2 * width
+    y_offset_5_4 = base + 3 * width
+    y_offset_5_5 = base + 4 * width
+    #
+    base = 20
+    width = 36
+    y_offset_4_1 = base + 0 * width
+    y_offset_4_2 = base + 1 * width
+    y_offset_4_3 = base + 2 * width
+    y_offset_4_4 = base + 3 * width
+    breaks = baca.layout.Breaks(
+        baca.layout.Page(
+            1,
+            baca.layout.System(1, y_offset_3, distances=distances),
+            baca.layout.System(4, y_offset_4, distances=distances),
+        ),
+        baca.layout.Page(
+            2,
+            baca.layout.System(8, y_offset_1, distances=distances),
+            baca.layout.System(10, y_offset_4, distances=distances),
+        ),
+        baca.layout.Page(
+            3,
+            baca.layout.System(12, y_offset_1, distances=distances),
+            baca.layout.System(17, y_offset_3 - half, distances=distances),
+            baca.layout.System(21, y_offset_4, distances=distances),
+        ),
+        baca.layout.Page(
+            4,
+            baca.layout.System(23, y_offset_4_1, distances=distances),
+            baca.layout.System(27, y_offset_4_2, distances=distances),
+            baca.layout.System(30, y_offset_4_3, distances=distances),
+            baca.layout.System(33, y_offset_4_4, distances=distances),
+        ),
+        baca.layout.Page(
+            5,
+            baca.layout.System(36, y_offset_5_1, distances=distances),
+            baca.layout.System(37, y_offset_5_2, distances=distances),
+            baca.layout.System(39, y_offset_5_3, distances=distances),
+            baca.layout.System(43, y_offset_5_4, distances=distances),
+            baca.layout.System(47, y_offset_5_5, distances=distances),
+        ),
+        baca.layout.Page(
+            6,
+            baca.layout.System(50, y_offset_1, distances=distances),
+            baca.layout.System(52, y_offset_4, distances=distances),
+            baca.layout.System(56, y_offset_5, distances=distances),
+            baca.layout.System(61, y_offset_6, distances=distances),
+        ),
+        baca.layout.Page(
+            7,
+            baca.layout.System(66, y_offset_1, distances=distances),
+            baca.layout.System(69, y_offset_3, distances=distances),
+            baca.layout.System(71, y_offset_4, distances=distances),
+            baca.layout.System(76, y_offset_5, distances=distances),
+            baca.layout.System(79, y_offset_6, distances=distances),
+        ),
+        baca.layout.Page(
+            8,
+            baca.layout.System(82, y_offset_1, distances=distances),
+            baca.layout.System(85, y_offset_2, distances=distances),
+            baca.layout.System(88, y_offset_3, distances=distances),
+            baca.layout.System(92, y_offset_4, distances=distances),
+            baca.layout.System(94, y_offset_5, distances=distances),
+            baca.layout.System(96, y_offset_6, distances=distances),
+        ),
+        baca.layout.Page(
+            9,
+            baca.layout.System(100, y_offset_2, distances=distances),
+            baca.layout.System(103, y_offset_3, distances=distances),
+            baca.layout.System(105, y_offset_4, distances=distances),
+            baca.layout.System(106, y_offset_5 + half, distances=distances),
+        ),
+        baca.layout.Page(
+            10,
+            baca.layout.System(107, y_offset_4_1, distances=distances),
+            baca.layout.System(110, y_offset_4_2, distances=distances),
+            baca.layout.System(113, y_offset_4_3, distances=distances),
+            baca.layout.System(115, y_offset_4_4, distances=distances),
+        ),
+        baca.layout.Page(
+            11,
+            baca.layout.System(117, y_offset_1 + 6.5, distances=distances),
+            baca.layout.System(121, y_offset_2, distances=distances),
+            baca.layout.System(123, y_offset_5 - half, distances=distances),
+            baca.layout.System(126, y_offset_6 - 2, distances=distances),
+        ),
+    )
+    spacing_dictionary = {
+        1: (1, 12),
+        8: (1, 16),
+        11: (1, 16),
+        22: (1, 16),
+        36: "natural",
+        50: (1, 12),
+        51: (1, 12),
+        92: (1, 12),
+        93: (1, 12),
+        94: (1, 12),
+        95: (1, 12),
+        104: (1, 12),
+        112: (1, 12),
+        116: (1, 12),
+    }
+    return baca.section.make_layout_score(
+        breaks,
+        environment.time_signatures[:128],
+        spacing_dictionary=spacing_dictionary,
+    )
+
+
+if __name__ == "__main__":
+    environment = baca.build.read_build_directory_environment()
+    lilypond_file, bol_measure_numbers = main(environment)
+    baca.build.persist_build_layout_ily(environment.build_directory, lilypond_file)
+    baca.build.write_bol_metadata(environment.build_directory, bol_measure_numbers)
---
+++ wttc/builds/letter-parts/guitar-1/positioning.ily
@@ -0,0 +1,91 @@
+\version "2.25.16"
+
+% SCENE 1
+
+\markup scene-i-B-parts-positioning = \markup
+  \override #'(line-width . 110)
+  \translate #'(-76 . -25)
+  \etc
+\markup scene-i-C-parts-positioning = \markup \translate #'(-14 . -30) \etc
+\markup scene-i-D-parts-positioning = \markup \translate #'(-16 . -32) \etc
+
+% SCENE 2
+
+\markup scene-ii-title-parts-positioning = \markup \translate #'(-8 . 8) \etc
+\markup scene-ii-A-parts-positioning = \markup \translate #'(-10 . -18) \etc
+\markup scene-ii-B-parts-positioning = \markup \translate #'(0 . -18) \etc
+\markup scene-ii-C-parts-positioning = \markup \translate #'(0 . -15) \etc
+\markup scene-ii-D-parts-positioning = \markup \translate #'(0 . -15) \etc
+\markup scene-ii-E-parts-positioning = \markup \translate #'(0 . -15) \etc
+\markup scene-ii-F-parts-positioning = \markup
+  \override #'(line-width . 20)
+  \translate #'(0 . -15)
+  \etc
+\markup scene-ii-G-parts-positioning = \markup \translate #'(0 . -13) \etc
+\markup scene-ii-H-parts-positioning = \markup \translate #'(-15 . -42.5) \etc
+
+% SCENE 3
+
+\markup scene-iii-title-parts-positioning = \markup \translate #'(-10 . 16) \etc
+\markup scene-iii-A-parts-positioning = \markup \translate #'(-31 . -22) \etc
+\markup scene-iii-B-parts-positioning = \markup
+  \override #'(line-width . 82)
+  \translate #'(-52 . -20)
+  \etc
+\markup scene-iii-C-parts-positioning = \markup
+  \override #'(line-width . 80)
+  \translate #'(-22 . -25)
+  \etc
+\markup scene-iii-D-parts-positioning = \markup \translate #'(-22 . -44) \etc
+\markup scene-iii-E-parts-positioning = \markup \translate #'(-50 . -20) \etc
+\markup scene-iii-F-parts-positioning = \markup \translate #'(9 . -15) \etc
+
+% SCENE 4
+
+\markup scene-iv-title-parts-positioning = \markup \translate #'(-8 . 34) \etc
+\markup scene-iv-A-parts-positioning = \markup \translate #'(24 . 26) \etc
+\markup scene-iv-B-parts-positioning = \markup \translate #'(0 . -13) \etc
+\markup scene-iv-C-parts-positioning = \markup \translate #'(0 . -13) \etc
+\markup scene-iv-D-parts-positioning = \markup \translate #'(0 . -13) \etc
+\markup scene-iv-E-parts-positioning = \markup \translate #'(0 . -13) \etc
+\markup scene-iv-F-parts-positioning = \markup \translate #'(0 . -13) \etc
+\markup scene-iv-G-parts-positioning = \markup \translate #'(0 . -15) \etc
+\markup scene-iv-I-parts-positioning = \markup
+  \override #'(line-width . 36)
+  \translate #'(26 . -15)
+  \etc
+\markup scene-iv-J-parts-positioning = \markup
+  \override #'(line-width . 36)
+  \translate #'(72 . -15)
+  \etc
+\markup scene-iv-K-parts-positioning = \markup \translate #'(42 . -13) \etc
+
+% SCENE 5
+
+\markup scene-v-title-parts-positioning = \markup \translate #'(-8 . 16) \etc
+\markup scene-v-A-parts-positioning = \markup \translate #'(0 . -13) \etc
+\markup scene-v-B-parts-positioning = \markup
+  \override #'(line-width . 24)
+  \translate #'(-1 . -13)
+  \etc
+\markup scene-v-C-parts-positioning = \markup \translate #'(0 . -15) \etc
+
+% SCENE 6
+
+\markup scene-vi-title-parts-positioning = \markup \translate #'(-8 . 15) \etc
+\markup scene-vi-A-parts-positioning = \markup \translate #'(30 . 10) \etc
+\markup scene-vi-B-parts-positioning = \markup \translate #'(-10 . -15) \etc
+\markup scene-vi-C-parts-positioning = \markup \translate #'(-10 . -15) \etc
+\markup scene-vi-D-parts-positioning = \markup \translate #'(0 . -17) \etc
+\markup scene-vi-E-parts-positioning = \markup
+  \override #'(baseline-skip . 3)
+  \override #'(font-size . 0)
+  \override #'(line-width . 50)
+  \translate #'(-28 . -26)
+  \etc
+\markup scene-vi-F-parts-positioning = \markup
+  \override #'(baseline-skip . 3)
+  \override #'(font-size . 0)
+  \translate #'(-42 . -18)
+  \etc
+\markup scene-vi-G-parts-positioning = \markup \translate #'(-52 . -17.5) \etc
